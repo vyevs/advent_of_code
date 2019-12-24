6958d8843a083c7f0a9767dc6a90c5e8bdd3c79d
Vladimir Yevseenko <yevseenko.vladimir@gmail.com>
2019-12-23 19:05:22 -0700

    solved 2015/day11

--- 2015/day11/part1.go
+++ 2015/day11/part1.go
@@ -3,17 +3,58 @@ package main
 import "fmt"
 
 func main() {
-	src := "zzy"
-	pw := pw{
-		bs:    make([]byte, 64),
-		bsLen: 64,
-		pwLen: len(src),
-	}
-	for i := range src {
-		pw.bs[64-len(src)+i] = src[i]
+	src := "cqjxjnds"
+
+	pw := pwFromString(src)
+
+	for pw = next(pw); !legalPassword(pw); pw = next(pw) {
 	}
 
-	fmt.Println(string(pw.bs[pw.bsLen-pw.pwLen:]))
+	fmt.Println(pw)
+
+	for pw = next(pw); !legalPassword(pw); pw = next(pw) {
+	}
+
+	fmt.Println(pw)
+}
+
+func legalPassword(pw pw) bool {
+	return !containsIllegalLetters(pw) && containsRun(pw) && contains2Pairs(pw)
+}
+
+func containsRun(pw pw) bool {
+	for i := pw.bsLen - pw.pwLen; i < pw.bsLen-2; i++ {
+		if pw.bs[i+1] == pw.bs[i]+1 && pw.bs[i+2] == pw.bs[i+1]+1 {
+			return true
+		}
+	}
+	return false
+}
+
+func containsIllegalLetters(pw pw) bool {
+	for _, b := range pw.bs[pw.bsLen-pw.pwLen:] {
+		if b == 'i' || b == 'o' || b == 'l' {
+			return true
+		}
+	}
+	return false
+}
+
+func contains2Pairs(pw pw) bool {
+	var firstPair byte
+	for i := pw.bsLen - pw.pwLen; i < pw.bsLen-1; {
+		if pw.bs[i] == pw.bs[i+1] {
+			if firstPair == 0 {
+				firstPair = pw.bs[i]
+				i += 2
+			} else if pw.bs[i] != firstPair {
+				return true
+			}
+		} else {
+			i++
+		}
+	}
+	return false
 }
 
 type pw struct {
@@ -22,17 +63,37 @@ type pw struct {
 	pwLen int
 }
 
+func pwFromString(s string) pw {
+	pw := pw{
+		bs:    make([]byte, 64),
+		bsLen: 64,
+		pwLen: len(s),
+	}
+	for i := range s {
+		pw.bs[64-len(s)+i] = s[i]
+	}
+	return pw
+}
+
+func (pw pw) String() string {
+	return string(pw.bs[pw.bsLen-pw.pwLen:])
+}
+
 func next(pw pw) pw {
 
-	var carry byte
-	for i := pw.bsLen - 1; ; i-- {
+	carry := true
+	for i := pw.bsLen - 1; i >= pw.bsLen-pw.pwLen && carry; i-- {
 		pw.bs[i]++
 		if pw.bs[i] > 'z' {
 			pw.bs[i] = 'a'
-			carry = 1
 		} else {
-			break
+			carry = false
 		}
+	}
+
+	if carry {
+		pw.pwLen++
+		pw.bs[pw.bsLen-pw.pwLen] = 'a'
 	}
 
 	return pw
